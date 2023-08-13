1bab2545bbfe9d132442ef350a687f046474beab
CristianZerpa <cristian.zerpa@mercadolibre.com>
2023-08-13 07:15:19 -0300

    Spring Initializr

---
+++ src/test/java/ar/com/zsoft/pokemon/PokemonApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.zsoft.pokemon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
